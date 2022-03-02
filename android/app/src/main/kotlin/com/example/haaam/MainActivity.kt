92f52da83b2a8d61b2ed48382a246f2c2a2e0166
sieukim <s2lver@naver.com>
2022-03-03 04:47:50 +0900

    web build 결과물 추가

--- android/app/src/main/kotlin/com/example/haaam/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.haaam
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/globalVariable.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class GlobalVariable {
-  static final GlobalKey<NavigatorState> navigatorState =
-      GlobalKey<NavigatorState>();
-}
