b2f9b41f681392e431c5222f64f8b01b09a5f939
sieukim <s2lver@naver.com>
2022-03-03 03:51:29 +0900

    유사 알람 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:haaam/globalVariable.dart';
+import 'package:haaam/runAlarm.dart';
 
 import 'addPage.dart';
 import 'editPage.dart';
 import 'mainPage.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+  runAlarm();
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -29,7 +34,9 @@ class MyApp extends StatelessWidget {
         '/': (_) => const MainPage(),
         '/add': (_) => const AddPage(),
         '/edit': (_) => const EditPage(),
+        '/alarm': (_) => const RunAlarm(),
       },
+      navigatorKey: GlobalVariable.navigatorState,
     );
   }
 }
